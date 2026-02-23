405a39c1e1014076dc1d4f89f6135dfa042f45c5
Kristian Skov <kris701kj@gmail.com>
2026-02-23 07:28:42 +0100

    Made so you get medals for winning a game

--- Knuckle.Is.Bones.Core/Models/Saves/UserSaveDefinition.cs
+++ Knuckle.Is.Bones.Core/Models/Saves/UserSaveDefinition.cs
@@ -1,25 +1,27 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Knuckle.Is.Bones.Core.Models.Saves
 {
-	public class UserSaveDefinition<T>
+	public class UserSaveDefinition<T> where T : new()
 	{
-		public int AllTimeScore { get; set; }
-		public List<Guid> CompletedItems { get; set; }
-		public T UIData { get; set; }
-
-		public UserSaveDefinition(int allTimeScore, List<Guid> completedItems, T uIData)
-		{
-			AllTimeScore = allTimeScore;
-			CompletedItems = completedItems;
-			UIData = uIData;
-		}
+		public int AllTimeScore { get; set; } = 0;
+		public Dictionary<Guid, int> CompletedItems { get; set; } = new Dictionary<Guid, int>();
+		public T UIData { get; set; } = new T();
 
 		public void Save()
 		{
 			if (File.Exists("user.json"))
 				File.Delete("user.json");
 			File.WriteAllText("user.json", JsonSerializer.Serialize(this));
+		}
+
+		public void AppendCompletedItem(Guid id)
+		{
+			if (CompletedItems.ContainsKey(id))
+				CompletedItems[id]++;
+			else
+				CompletedItems.Add(id, 1);
 		}
 	}
 }
--- Knuckle.Is.Bones.OpenGL/Views/MainGameView/MainGame.cs
+++ Knuckle.Is.Bones.OpenGL/Views/MainGameView/MainGame.cs
@@ -121,22 +121,16 @@ namespace Knuckle.Is.Bones.OpenGL.Views.MainGameView
 				if ((Engine.State.FirstOpponent.Module is PlayerOpponentModule) && (Engine.State.SecondOpponent.Module is not PlayerOpponentModule) && Engine.State.Winner == Engine.State.FirstOpponent.Module.OpponentID)
 				{
 					pointsGained = (int)(Engine.State.FirstOpponentBoard.GetValue() * Engine.State.SecondOpponent.Difficulty);
-					if (!Parent.User.CompletedItems.Contains(Engine.State.SecondOpponent.ID))
-						Parent.User.CompletedItems.Add(Engine.State.SecondOpponent.ID);
-					if (!Parent.User.CompletedItems.Contains(Engine.State.FirstOpponentBoard.ID))
-						Parent.User.CompletedItems.Add(Engine.State.FirstOpponentBoard.ID);
-					if (!Parent.User.CompletedItems.Contains(Engine.State.CurrentDice.ID))
-						Parent.User.CompletedItems.Add(Engine.State.CurrentDice.ID);
+					Parent.User.AppendCompletedItem(Engine.State.SecondOpponent.ID);
+					Parent.User.AppendCompletedItem(Engine.State.FirstOpponentBoard.ID);
+					Parent.User.AppendCompletedItem(Engine.State.CurrentDice.ID);
 				}
 				if ((Engine.State.SecondOpponent.Module is PlayerOpponentModule) && (Engine.State.FirstOpponent.Module is PlayerOpponentModule) && Engine.State.Winner == Engine.State.SecondOpponent.Module.OpponentID)
 				{
 					pointsGained = (int)(Engine.State.SecondOpponentBoard.GetValue() * Engine.State.FirstOpponent.Difficulty);
-					if (!Parent.User.CompletedItems.Contains(Engine.State.FirstOpponent.ID))
-						Parent.User.CompletedItems.Add(Engine.State.FirstOpponent.ID);
-					if (!Parent.User.CompletedItems.Contains(Engine.State.FirstOpponentBoard.ID))
-						Parent.User.CompletedItems.Add(Engine.State.FirstOpponentBoard.ID);
-					if (!Parent.User.CompletedItems.Contains(Engine.State.CurrentDice.ID))
-						Parent.User.CompletedItems.Add(Engine.State.CurrentDice.ID);
+					Parent.User.AppendCompletedItem(Engine.State.FirstOpponent.ID);
+					Parent.User.AppendCompletedItem(Engine.State.FirstOpponentBoard.ID);
+					Parent.User.AppendCompletedItem(Engine.State.CurrentDice.ID);
 				}
 
 				Parent.User.AllTimeScore += pointsGained;
