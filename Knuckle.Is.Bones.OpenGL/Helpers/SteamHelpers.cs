e4e5325a8e6fe27f856b7a3efa474194ce313a10
Kristian Skov <kris701kj@gmail.com>
2026-02-24 10:07:17 +0100

    Fixed so that the application wont crash if steam is not open

---
+++ Knuckle.Is.Bones.OpenGL/Helpers/SteamHelpers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Knuckle.Is.Bones.OpenGL.Helpers
+{
+	public static class SteamHelpers
+	{
+		public static bool IsInitialized = false;
+	}
+}
