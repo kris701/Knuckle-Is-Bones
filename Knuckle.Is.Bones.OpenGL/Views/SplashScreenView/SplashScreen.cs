cd692ace2fee0eceaa067970a5765d23b5714cf2
Kristian Skov <kris701kj@gmail.com>
2026-02-24 16:43:30 +0100

    Made the splashscreen skippable with any input

--- Knuckle.Is.Bones.OpenGL/Views/SplashScreenView/SplashScreen.cs
+++ Knuckle.Is.Bones.OpenGL/Views/SplashScreenView/SplashScreen.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using MonoGame.OpenGL.Formatter.Input;
 using System;
+using System.Linq;
 
 namespace Knuckle.Is.Bones.OpenGL.Views.SplashScreenView
 {
@@ -11,13 +12,13 @@ namespace Knuckle.Is.Bones.OpenGL.Views.SplashScreenView
 	{
 		public static Guid ID = new Guid("20aed282-f7e1-4595-9f53-85d95c0efe6f");
 
-		private readonly KeyWatcher _enterKeyWatcher;
+		private readonly KeysAnyWatcher _skipKeyWatcher;
 		private readonly GameTimer _splashTimer;
 
 		public SplashScreen(KnuckleBoneWindow parent) : base(parent, ID)
 		{
 			parent.Audio.PlaySong(new Guid("9b223250-88f1-4c3e-9d80-0c4087ab9369"));
-			_enterKeyWatcher = new KeyWatcher(Keys.Enter, () => SwitchView(new MainMenu(parent)));
+			_skipKeyWatcher = new KeysAnyWatcher(Enum.GetValues<Keys>().ToList(), () => SwitchView(new MainMenu(parent)));
 			_splashTimer = new GameTimer(TimeSpan.FromSeconds(3), (t) => SwitchView(new MainMenu(parent)));
 			Initialize();
 		}
@@ -25,7 +26,7 @@ namespace Knuckle.Is.Bones.OpenGL.Views.SplashScreenView
 		public override void OnUpdate(GameTime gameTime)
 		{
 			var keyState = Keyboard.GetState();
-			_enterKeyWatcher.Update(keyState);
+			_skipKeyWatcher.Update(keyState);
 			_splashTimer.Update(gameTime.ElapsedGameTime);
 		}
 	}
