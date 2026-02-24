f7002947dbf58f5315b697724cadb3450a85b71d
Kristian Skov <kris701kj@gmail.com>
2026-02-24 16:05:21 +0100

    Added the ability to press the ESC key to go back from any menu

--- Knuckle.Is.Bones.OpenGL/Views/HowToPlayView/HowToPlay.cs
+++ Knuckle.Is.Bones.OpenGL/Views/HowToPlayView/HowToPlay.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using Knuckle.Is.Bones.OpenGL.Views.MainMenuView;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.OpenGL.Formatter.Input;
+using System;
 
 namespace Knuckle.Is.Bones.OpenGL.Views.HowToPlayView
 {
 	public partial class HowToPlay : BaseKnuckleBoneFadeView
 	{
 		public static Guid ID = new Guid("207df693-09c5-428c-b2ab-513950bf6bd0");
+		private readonly KeyWatcher _escapeKeyWatcher;
 		public HowToPlay(KnuckleBoneWindow parent) : base(parent, ID)
 		{
+			_escapeKeyWatcher = new KeyWatcher(Keys.Escape, () => SwitchView(new MainMenu(parent)));
 			Initialize();
+		}
+
+		public override void OnUpdate(GameTime gameTime)
+		{
+			var keyState = Keyboard.GetState();
+			_escapeKeyWatcher.Update(keyState);
 		}
 	}
 }
--- Knuckle.Is.Bones.OpenGL/Views/SettingsMenuView/SettingsMenu.cs
+++ Knuckle.Is.Bones.OpenGL/Views/SettingsMenuView/SettingsMenu.cs
@@ -1,6 +1,10 @@
 ﻿using Knuckle.Is.Bones.OpenGL.Models;
 using Knuckle.Is.Bones.OpenGL.Screens.SettingsView.AcceptView;
+using Knuckle.Is.Bones.OpenGL.Views.MainMenuView;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using MonoGame.OpenGL.Formatter.Controls;
+using MonoGame.OpenGL.Formatter.Input;
 using System;
 
 namespace Knuckle.Is.Bones.OpenGL.Views.SettingsMenuView
@@ -10,20 +14,38 @@ namespace Knuckle.Is.Bones.OpenGL.Views.SettingsMenuView
 		public static Guid ID = new Guid("356b5d18-1aaf-4c98-aa73-2b27fe82ed1f");
 
 		private readonly SettingsDefinition _newSettings;
+		private readonly KeyWatcher _escapeKeyWatcher;
 
 		public SettingsMenu(KnuckleBoneWindow parent) : base(parent, ID)
 		{
 			_newSettings = Parent.Settings.Clone();
+			_escapeKeyWatcher = new KeyWatcher(Keys.Escape, Escape);
 			Initialize();
 		}
 
-		private void OnSaveAndApplySettings(ButtonControl sender)
+		private void OnSaveAndApplySettings(ButtonControl sender) => ShowAcceptView();
+
+		private void ShowAcceptView()
 		{
 			var oldSettings = Parent.Settings.Clone();
 			var newSettings = _newSettings.Clone();
 			Parent.Settings = newSettings;
 			Parent.ApplySettings();
 			SwitchView(new AcceptView(Parent, oldSettings, newSettings));
+		}
+
+		public override void OnUpdate(GameTime gameTime)
+		{
+			var keyState = Keyboard.GetState();
+			_escapeKeyWatcher.Update(keyState);
+		}
+
+		private void Escape()
+		{
+			if (Parent.Settings.Equals(_newSettings))
+				SwitchView(new MainMenu(Parent));
+			else
+				ShowAcceptView();
 		}
 	}
 }
